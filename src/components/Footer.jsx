56ed584ec753b2d7289a25f1c36711d97a493520
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-14 19:44:11 -0400

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+function Footer() {
+  return (
+    <Flex justifyContent="center" alignItems="center" h="40px">
+      <Text>nronzel &copy; 2023 All Rights Reserved</Text>
+    </Flex>
+  );
+}
+
+export default Footer;
