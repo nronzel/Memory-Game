f93fa61930aa680ee710f6b29b04d1aaca4367c2
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-14 19:45:33 -0400

    added stats to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Flex, Heading, Button, useColorMode } from "@chakra-ui/react";
+import { Flex, Heading, useColorModeValue, StatGroup } from "@chakra-ui/react";
 import ThemeSwitcher from "./ThemeSwitcher";
+import Stats from "./Stats";
 
-function Header() {
+function Header({ score, round }) {
   return (
     <Flex
       justifyContent="center"
       alignItems="center"
       p={2}
       borderBottom="1px solid"
+      bgColor={useColorModeValue("blue.200", "blue.900")}
     >
       <Flex w="80%" justifyContent="space-between" alignItems="center">
         <Heading>Memory Game</Heading>
+        <StatGroup gap={8}>
+          <Stats title="Highscore" stat={score} />
+          <Stats title="Round" stat={round} />
+        </StatGroup>
         <ThemeSwitcher />
       </Flex>
     </Flex>
